961e774f924d2fb79efb14dd204bf5804adf5dfd
Nicolas <pelusonicolas7@gmail.com>
2023-07-18 01:41:36 -0300

    front Primeiros passos

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Style from "./Header.module.css"
+
+function Header(){
+    return (
+        <header className={Style.Header}>
+            <nav>
+                
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
+
